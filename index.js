ef03d00df446f80a29544321532ff02c172f3ca7
shaheemux <120124594+shaheemux@users.noreply.github.com>
2023-11-24 09:46:45 +0200

    added files

--- index.js
+++ index.js
@@ -1,14 +1,53 @@
-function countTruthy() {
-    let valuesInput = document.getElementById("values");
-    let values = valuesInput.value.split(",");
-  
-    let count = 0;
-    values.forEach(function(value) {
-      if (value.trim()) {
-        count++;
-      }
-    });
-  
-    let result = document.getElementById("result");
-    result.innerHTML = "The count of truthy values is: " + count;
+(function() {
+  // Getting the references to the required elements
+  const convertButton = document.getElementById('convertButton');
+  const resultElement = document.getElementById('result');
+  const inputElement = document.getElementById('input');
+
+  // Attach event listener to the convert button
+  convertButton.addEventListener('click', handleClick);
+
+  // Function to handle the button click event
+  function handleClick() {
+      const inputValue = inputElement.value;
+      const truthyCount = countTruthy(inputValue);
+      displayResult(truthyCount);
   }
+
+  // Function to count the truthy values
+  function countTruthy(value) {
+      let count = 0;
+
+      // Check if the value is truthy
+      if (value) {
+          if (typeof value === 'string') {
+              count = 1;
+          } else if (typeof value === 'object') {
+              for (const key in value) {
+                  if (value[key]) {
+                      count++;
+                  }
+              }
+          } else {
+              count = 1;
+          }
+      }
+
+      return count;
+  }
+
+  // Function to display the result
+  function displayResult(count) {
+      const resultText = 'Truthy count: ' + formatCount(count);
+      resultElement.textContent = resultText;
+  }
+
+  // Function to format the count with leading zeros if necessary
+  function formatCount(count) {
+      if (count < 10) {
+          return '0' + count;
+      } else {
+          return count.toString();
+      }
+  }
+})();
